9c8ba95753590f498495690e5633a40a5a9d9e2d
Tornyai Laurentiu Andrei <121413180+thedrew94@users.noreply.github.com>
2023-02-14 10:08:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+
+//ALERT PROJECT INFO
+alert("This project is a replica of the official Hakone Navi website.\n\
+It was created with the goal of refining my skills in JavaScript, HTML, and CSS.\n\
+Some features are still under development and may differ from the original.Not yet 100% implemented for phone\n\
+This project was completed by Tornyai Laurentiu Andrei.");
+
 //MAP DISPLAY
 
 const map = L.map('map').setView([35.227764, 139.032011], 13);
