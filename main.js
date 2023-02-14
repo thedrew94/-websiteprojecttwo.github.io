7bfd0577d56767ecdf8f022c81d591191c51ada2
Tornyai Laurentiu Andrei <121413180+thedrew94@users.noreply.github.com>
2023-02-14 10:06:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//MAP DISPLAY
+
+const map = L.map('map').setView([35.227764, 139.032011], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([35.227764, 139.032011]).addTo(map)
+    .bindPopup('Welcome to Hakone')
+    .openPopup();
+    
+
+// 35.227764, 139.032011
